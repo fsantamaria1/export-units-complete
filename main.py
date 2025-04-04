c1a5b8ea595344c29581eb716da247f876aa5b72
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2025-04-04 13:15:32 -0400

    Create boilerplate files

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+This script will to run daily and create a CSV from data from the MS SQL database.
+"""
+
+def main():
+    """
+    Main function to run the script.
+    """
+
+if __name__ == "__main__":
+    main()
