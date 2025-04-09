115ec4aa7d2792b25a89c8f683d38b28c833c40c
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2025-04-09 11:05:09 -0400

    Add functionality to load environment variables

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 """
 This script will to run daily and create a CSV from data from the MS SQL database.
 """
+from os.path import dirname, join
+from dotenv import load_dotenv
+
 
 def main():
     """
     Main function to run the script.
     """
+
+    # Load environment variables
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
 
 if __name__ == "__main__":
     main()
