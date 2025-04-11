15a49d759ca26f5e02528a41afcf49eb087b6e1d
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2025-04-11 13:19:38 -0400

    Import and use the run_stored_procedure function from the db_functions module

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 """
 This script will to run daily and create a CSV from data from the MS SQL database.
 """
+import os
 from os.path import dirname, join
 from dotenv import load_dotenv
+from resources.db_functions import run_stored_procedure
 
 
 def main():
@@ -13,6 +15,8 @@ def main():
     # Load environment variables
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
+
+    run_stored_procedure(os.environ.get('schema_name'), os.environ.get('stored_procedure_name'))
 
 
 if __name__ == "__main__":
