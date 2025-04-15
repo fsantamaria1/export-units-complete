73611931c5fab493d00440e5a4b69f67e7fdba74
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2025-04-15 10:23:22 -0400

    Create the units complete export model

--- resources/models.py
+++ resources/models.py
@@ -1,6 +1,71 @@
 """
 This module contains the models for the database.
 """
+import os
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, VARCHAR, DATE, NVARCHAR, NUMERIC, DATETIME
 
 Base = declarative_base()
+
+
+class UnitsCompleteExport(Base):
+    """
+    A class that represents the UnitsCompleteExport table in the database.
+    """
+    __tablename__ = 'UnitsCompleteExport'
+    __table_args__ = {'schema': os.environ.get('schema_name', 'dbo')}
+
+    export_id = Column(Integer, primary_key=True)
+    job_number = Column(VARCHAR(10), nullable=False)
+    job_date = Column(DATE, nullable=False)
+    phase_number = Column(NVARCHAR(5), nullable=False)
+    category_number = Column(NVARCHAR(15), nullable=False)
+    unit_change = Column(NUMERIC(8, 2), nullable=False)
+    timesheet_id = Column(Integer, nullable=True)
+    change_order_id = Column(Integer, nullable=True)
+    sub_report_id = Column(Integer, nullable=True)
+    vendor_name = Column(NVARCHAR(30), nullable=True)
+    date_created = Column(DATETIME, nullable=True)
+
+    def to_dict(self):
+        """
+        Convert the UnitsCompleteExport object to a dictionary.
+        :return: A dictionary representation of the UnitsCompleteExport object.
+        """
+        fields = ['job_date', 'job_number', 'phase_number', 'category_number', 'unit_change']
+        data = {field: getattr(self, field) for field in fields}
+        data['notes'] = self.get_notes()
+
+        return data
+
+    def get_notes(self):
+        """
+        Returns the notes for the UnitsCompleteExport object.
+        """
+        notes = ""
+        if self.timesheet_id:
+            notes += f"Timesheet ID: {self.timesheet_id} "
+        if self.change_order_id:
+            notes += f"Change Order ID: {self.change_order_id} "
+        if self.sub_report_id:
+            notes += f"Sub Report ID: {self.sub_report_id} "
+        if self.vendor_name:
+            notes += f"Vendor Name: {self.vendor_name} "
+        return notes
+
+    def __repr__(self):
+        """
+        Return a string representation of the UnitsCompleteExport object.
+        :return: A string representation of the UnitsCompleteExport object.
+        """
+        return (f"<UnitsCompleteExport(export_id={self.export_id}, "
+                f"job_number={self.job_number}, "
+                f"job_date={self.job_date}, "
+                f"phase_number={self.phase_number}, "
+                f"category_number={self.category_number}, "
+                f"unit_change={self.unit_change}, "
+                f"timesheet_id={self.timesheet_id}, "
+                f"change_order_id={self.change_order_id}, "
+                f"sub_report_id={self.sub_report_id}, "
+                f"vendor_name={self.vendor_name}, "
+                f"date_created={self.date_created})>")
